56a52cb41bd15068f54f54a233492e94bfcf542a
Alexandr <asokolov19996@gmail.com>
2023-08-13 16:31:52 +0600

    Deleted unuseful lines

--- fixture/application.py
+++ fixture/application.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 
-from pages.book_page import BookPageHelper
 from pages.home_page import HomePageHelper
-from pages.payment_page import PaymentPageHelper
-from pages.session_page import SessionPageHelper
 from pages.ticket_page import TicketPageHelper
 
 
@@ -11,16 +8,13 @@ class Application:
 
     def __init__(self):
         driver = webdriver.Chrome()
-        driver.get("https://example-stage.team/")
+        driver.get("https://examplelink-stage.team/")
 
         self.driver = driver
         self.driver.implicitly_wait(40)
         self.driver.maximize_window()
         self.home_page = HomePageHelper(self)
         self.ticket_page = TicketPageHelper(self)
-        self.book_page = BookPageHelper(self)
-        self.payment_page = PaymentPageHelper(self)
-        self.session_page = SessionPageHelper(self)
 
     def destroy(self):
         self.driver.quit()
